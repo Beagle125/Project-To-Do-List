bc4a91a03d2e05241158b145654e3aa89a26067e
Cedric <cedric_daniel_chan@dlsu.edu.ph>
2026-05-07 20:38:06 +0800

    Change the default data and create a module where all the class logic is defined

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import "./styles.css";
 import * as myModules from './myModules.js';
 
-// The initial setUp when loading the website logic
+// This IIFE contains all the logic for the website, where it all starts
 (function websiteLogic (){
     const mainContainer = document.getElementById('content'); // the main container
     let storage = localStorage.getItem('donezoData');
 
 
+    // The initial setUp when loading the website logic
     const setUp = (mainContainer) => {
         myModules.LSMCheckStorage(storage);
         myModules.DMMcreateSideBar(mainContainer);
     };
 
+
+    // Start of the website
     setUp();
+
 })();
--- src/localStorageModule.js
+++ src/localStorageModule.js
@@ -1,8 +1,10 @@
+import { Project } from "./classLogicModule";
+
 const LSMCheckStorage = (storage) => {
     /* Intial check of storage item*/
     if (!storage){
-        let defaultData = [0];
-        localStorage.setItem('donezoData', defaultData);
+        let defaultData = new Project(self.crypto.randomUUID(), "Project 1", []);
+        localStorage.setItem('donezoData', JSON.stringify(defaultData));
     }
 }
 
