71e7ec358923a25b3cd94d926f39d71f4066c62d
Cedric <cedric_daniel_chan@dlsu.edu.ph>
2026-05-07 19:23:15 +0800

    Add the logo in the static folder and create the color palette of the website

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,14 @@ export default {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       }
     ],
   },
